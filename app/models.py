7ca5d46bd802a20ba12937abe66d99b31c7f3c90
IndraGultom <gultomindra21@gmail.com>
2024-05-08 23:34:31 +0800

    Posts now has a sliding bar and a javascript showing the user guess price, things are now logged, errors page handled, users now has an added field 'points'

--- app/models.py
+++ app/models.py
@@ -22,6 +22,8 @@ class User(UserMixin, db.Model):
     
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
+    
+    points: so.Mapped[int] = so.mapped_column(sa.Integer)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
