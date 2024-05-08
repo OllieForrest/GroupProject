3aead35c9ca21f14bb7ad11c321c12b270720eb4
IndraGultom <gultomindra21@gmail.com>
2024-05-08 20:35:04 +0800

    Added some error handling page and logging

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
-from config import UPLOAD_FOLDER
-from flask import send_from_directory
+import logging
+from logging.handlers import SMTPHandler
 
 
 app = Flask(__name__)
@@ -15,14 +15,28 @@ login = LoginManager(app)
 login.login_view = 'login'
 app.config['SECRET_KEY'] = 'you_will_never_ever_guess_it'
 
-#What extension is allowed for the upload
+#extension is allowed for the upload
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
-#Upload folder and max content size
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-from app import routes, models
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+from app import routes, models, errors
 
 
