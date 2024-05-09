56560e7d3942473e2a12d8e2d5461514d84da866
jesshc12 <23403545@student.uwa.edu.au>
2024-05-09 12:43:55 +0800

    jess

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/indragultom/microblog/venv"
+set -gx VIRTUAL_ENV "/Users/jessicaharcourt-cooke/Desktop/GroupProject/GroupProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
